877ef2cdbb93008e6e523e5a3c478275038836d9
Zaranik Bogdan <zaranikBZ@gmail.com>
2020-11-09 19:39:34 +0200

    Добавьте файлы проекта.

---
+++ Lab3py.py
@@ -0,0 +1,18 @@
+#для pow 
+import math
+#константа точности
+EPS=1e-5
+#инициализация
+xPrev=1.0
+xCur=0.2
+#основной цикл
+while(abs(xPrev-xCur)>=EPS):
+    xPrev=xCur
+    xCur=(2-pow(xPrev, 3))/5
+    print("xPrev = %7.5f\t"%xPrev, end="")
+    print("xCur = %7.5f"%xCur, end="\n")
+#вывод ответа
+ans=xCur
+print("Answer is : %7.5f"%ans)
+    
+
